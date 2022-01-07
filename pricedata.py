045932b933f06b7973c66cd59fb2e8eb54014c69
Jess <d1al.t0ne@outlook.com>
2022-01-07 17:26:04 -0500

    Removed User Agent

--- pricedata.py
+++ pricedata.py
@@ -9,8 +9,6 @@ PERIOD = "1y"
 INTERVAL = "1wk"
 
 def price_data(ticker):
-		session = requests_cache.CachedSession('yfinance.cache')
-		session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; rv:40.0) Gecko/20100101 Firefox/40.0'
 		symbol = yf.Ticker(ticker)
 		data = symbol.history(period=PERIOD, interval=INTERVAL)	
 		data = data[['Open', 'High', 'Low', 'Close']]
